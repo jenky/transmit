41d03ceda33c1ca9c8a190beee8aa796f8deb62b
Lynh <jenky.w0w@gmail.com>
2022-03-03 12:01:01 +0700

    Laravel 9

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,10 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
     ->name('*.php')
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
